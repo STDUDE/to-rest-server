5b23830b30596fdab5131f61b6482d7e2bc58cb5
Antony <antonina.klimovich@gmail.com>
2016-05-05 02:09:33 +0300

    test

--- webapp/webservice/urls.py
+++ webapp/webservice/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from . import views
+from webapp import views
+from webapp.webservice.v1.countries_select_request import CountriesReq
 
 urlpatterns = [
-    url(r'^countries/', views.countries, name='countries'),
-    url(r'^regions/(?P<country_id>[0-9]{6})/$', views.regions_select, name='regions'),
+    url(r'^countries/', CountriesReq.as_view(), name='countries'),
+    url(r'^regions/(?P<country_id>[0-9]{6})/$', views.countries, name='regions'),
     url(r'^cities/(?P<region_id>[0-9]{6})', views.countries, name='cities'),
 ]
--- webapp/webservice/v1/countries_select_request.py
+++ webapp/webservice/v1/countries_select_request.py
@@ -1,1 +1,24 @@
+import json
+
+from django.http import HttpResponse, HttpRequest
+from django.template.response import TemplateResponse
+from django.views.generic import View
+
+from webapp.dbmodel.models import Country
+
+class CountriesReq(View):
+    model = Country
+    def get(self, request, *args, **kwargs):
+        print("POST REQ CALLED")
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
+        content = body['content']
+        return content
+
+    def post(self, request, *args, **kwargs):
+        print("POST REQ CALLED")
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
+        content = body['content']
+        return content
 
